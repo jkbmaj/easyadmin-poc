df21364be34c6c665e32997184fd69b09e547013
Jakub Maj <jakub@jkbmaj.io>
2025-06-02 23:03:44 +0200

    Upgrade Symfony 6.3 to 6.4

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
